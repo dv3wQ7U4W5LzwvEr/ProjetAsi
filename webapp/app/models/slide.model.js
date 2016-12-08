3ae8f6c30222c82193648f01079c809eea30c030
BRETON Michael <michaelneuf@yahoo.fr>
2016-12-08 17:30:35 +0100

    create model for slide

--- webapp/app/models/slide.model.js
+++ webapp/app/models/slide.model.js
@@ -1,5 +1,8 @@
 "use strict";
 
+var CONFIG = JSON.parse(process.env.CONFIG);
+var fs = require("fs");
+
 function SlidModel(slidModel) {
 
     // Public
@@ -21,17 +24,41 @@ function SlidModel(slidModel) {
 
     static function create(slid, callback) {
 
+        var slidData = slid.getData();
+        fs.writeFile(slid.fileName, slidData);
+
+        var metadataPath = CONFIG.contentDirectory + "/" + slid.id + ".meta.json";
+        fs.writeFile(metadataPath, JSON.stringify(slid));
     }
 
     static function read(id, callback) {
 
+        var metadataPath = CONFIG.contentDirectory + "/" + id + ".meta.json";
+        fs.readFile(metadataPath, (err, data) => {
+            if (err) {
+                callback("Error reading metadata for id: " + id);
+            }
+
+            callback(null, new SlidModel(JSON.parse(data)));
+        });
     }
 
     static function update(slid, callback) {
 
+        var slidData = slid.getData();
+        var metadataPath = CONFIG.contentDirectory + "/" + slid.id + ".meta.json";
+
+        if(!slidData || slidData.length <= 0) {
+            callback("Error updating metadata for id: " + slid.id);
+        }
+
+        fs.writeFile(slid.fileName, slidData);
+        fs.writeFile(metadataPath, JSON.stringify(slid));
     }
 
-    static function delete(id, callback) {
-        
+    static function deletePres(id, callback) {
+
+        fs.unlink(CONFIG.presentationDirectory + "/" + id + ".pres.json");
+        fs.unlink(CONFIG.contentDirectory + "/" + slid.id + ".meta.json");
     }
 }
